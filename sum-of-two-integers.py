0edfc7212689378acf27a0c52dffb8b790406d42
Melita Dsouza <dmelita1994@gmail.com>
2018-03-08 23:54:01 -0500

    Update sum-of-two-integers.py

--- sum-of-two-integers.py
+++ sum-of-two-integers.py
@@ -5,9 +5,15 @@ class Solution(object):
         :type b: int
         :rtype: int
         """
-       
+         # 32 bits integer max
+        MAX = 0x7FFFFFFF
+        # 32 bits interger min
+        MIN = 0x80000000
+        # mask to get last 32 bits
+        mask = 0xFFFFFFFF
         while b:
             carry = a & b
-            a = a ^ b
-            b = carry << 1 
-        return a
+            a = (a ^ b)&mask
+            b = (carry << 1)&mask 
+            
+        return a if a <= MAX else ~(a ^ mask)
