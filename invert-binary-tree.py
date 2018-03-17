85fdbdafd6b2dc0eaccbc35129e87e0c135f115f
Melita Dsouza <dmelita1994@gmail.com>
2018-03-16 20:16:18 -0400

    Create invert-binary-tree.py

---
+++ invert-binary-tree.py
@@ -0,0 +1,42 @@
+#Time complexity: O(n), Space complexity: O(h)
+
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+#Solution using recursion
+class Solution(object):
+    def invertTree(self, root):
+        """
+        :type root: TreeNode
+        :rtype: TreeNode
+        """
+        if root is not None:
+            root.left, root.right = self.invertTree(root.right), self.invertTree(root.left)
+        return root
+
+#Solution using Stack
+class Solution1(object):
+    def invertTree(self, root):
+        """
+        :type root: TreeNode
+        :rtype: TreeNode
+        """
+    
+        
+        if root is not None:
+            nodes = []
+            nodes.append(root)
+            while nodes:
+                node = nodes.pop()
+                node.left, node.right = node.right, node.left
+                if node.left is not None:
+                    nodes.append(node.left)
+                if node.right is not None:
+                    nodes.append(node.right)
+
+        return root
+        
